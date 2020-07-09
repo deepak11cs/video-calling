7fc957dc1e7e665d95f2fdccb4366f860ac2c20d
deepak11cs <deepak.mca19.du@gmail.com>
2020-07-09 23:37:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const vidStyle = {
+  height : 500,
+  width :500,
+  margin: 5,
+  backgroundColor : 'black'
+}
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props){
+    super(props);
+    this.localVideoRef = React.createRef();
+    this.remoteVideoRef = React.createRef();
+    this.textref = React.createRef();
+  }
+
+  componentDidMount(){
+    this.pc = new RTCPeerConnection(null);
+    this.pc.onicecandidate = (e)=>{
+      if(e.candidate){
+        console.log(JSON.stringify(e.candidate));
+      }
+    }
+    this.pc.oniceconnectionstatechange = (e)=>{
+      console.log(e);
+    }
+    this.pc.ontrack = (e)=>{
+      this.remoteVideoRef.current.srcObject = e.streams[0];
+    }
+    const constraints = {audio: false,video: true};
+    let success = (stream)=>{
+      this.localVideoRef.current.srcObject = stream;
+      this.pc.addTrack = stream;
+    }
+    let failure = (error)=>{
+      console.log("getUserMedia error:",error);
+    }
+    navigator.mediaDevices.getUserMedia(constraints).then(success).catch(failure);
+  }
+
+  createOffer = ()=>{
+    this.pc.createOffer({offerToReceiveVideo: 1})
+      .then(sdp =>{
+        console.log(JSON.stringify(sdp));
+        this.pc.setLocalDescription(sdp);
+      },e=>{})
+  }
+
+  setRemoteDescription = ()=>{
+    const desc = JSON.parse(this.textref.value);
+    this.pc.setRemoteDescription(new RTCSessionDescription(desc));
+  }
+
+  createAnswer = ()=>{
+    console.log('answer');
+    this.pc.createAnswer({offerToReceiveVideo: 1})
+      .then(sdp => {
+        console.log(JSON.stringify(sdp));
+        this.pc.setLocalDescription(sdp);
+      },e=>{})
+  }
+
+  addCandidate = ()=>{
+    const candidate = JSON.parse(this.textref.value);
+    console.log("adding candidate",candidate);
+    this.pc.addIceCandidate(new RTCIceCandidate(candidate));
+  }
+
+  render(){
+    
+    return (
+      <div>
+        <video style={vidStyle} ref={this.localVideoRef} autoPlay></video>
+        <video style={vidStyle} ref={this.remoteVideoRef} autoPlay></video>
+
+        <br/>
+        <button onClick={this.createOffer}>Offer</button>
+        <button onClick={this.createAnswer}>Answer</button>
+        <br/>
+        <textarea ref={ref=>{this.textref = ref}}/>
+        <br/>
+        <button onClick={this.setRemoteDescription}>Set Remote desc</button>
+        <button onClick={this.addCandidate}>Add candidate</button>
+      </div>
+
+
+    );
+  }
+  
 }
 
 export default App;
