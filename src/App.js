b3484aa9575a831da54869985661b34f7f45071b
deepak11cs <deepak.mca19.du@gmail.com>
2020-08-11 04:29:49 +0530

    fixing server

--- src/App.js
+++ src/App.js
@@ -1,92 +1,29 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import CreateRoom from './routes/createRoom';
+import Room from './routes/room';
 const vidStyle = {
   height : 500,
   width :500,
   margin: 5,
   backgroundColor : 'black'
 }
-class App extends React.Component {
 
-  constructor(props){
-    super(props);
-    this.localVideoRef = React.createRef();
-    this.remoteVideoRef = React.createRef();
-    this.textref = React.createRef();
-  }
+function App(){
 
-  componentDidMount(){
-    this.pc = new RTCPeerConnection(null);
-    this.pc.onicecandidate = (e)=>{
-      if(e.candidate){
-        console.log(JSON.stringify(e.candidate));
-      }
-    }
-    this.pc.oniceconnectionstatechange = (e)=>{
-      console.log(e);
-    }
-    this.pc.ontrack = (e)=>{
-      this.remoteVideoRef.current.srcObject = e.streams[0];
-    }
-    const constraints = {audio: false,video: true};
-    let success = (stream)=>{
-      this.localVideoRef.current.srcObject = stream;
-      this.pc.addTrack = stream;
-    }
-    let failure = (error)=>{
-      console.log("getUserMedia error:",error);
-    }
-    navigator.mediaDevices.getUserMedia(constraints).then(success).catch(failure);
-  }
+  return (
 
-  createOffer = ()=>{
-    this.pc.createOffer({offerToReceiveVideo: 1})
-      .then(sdp =>{
-        console.log(JSON.stringify(sdp));
-        this.pc.setLocalDescription(sdp);
-      },e=>{})
-  }
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' exact component={CreateRoom}/>
+          <Route path="/room/:roomID" component={Room}/>
+        </Switch>
+      </BrowserRouter>
+    </div>
 
-  setRemoteDescription = ()=>{
-    const desc = JSON.parse(this.textref.value);
-    this.pc.setRemoteDescription(new RTCSessionDescription(desc));
-  }
+  )
 
-  createAnswer = ()=>{
-    console.log('answer');
-    this.pc.createAnswer({offerToReceiveVideo: 1})
-      .then(sdp => {
-        console.log(JSON.stringify(sdp));
-        this.pc.setLocalDescription(sdp);
-      },e=>{})
-  }
-
-  addCandidate = ()=>{
-    const candidate = JSON.parse(this.textref.value);
-    console.log("adding candidate",candidate);
-    this.pc.addIceCandidate(new RTCIceCandidate(candidate));
-  }
-
-  render(){
-    
-    return (
-      <div>
-        <video style={vidStyle} ref={this.localVideoRef} autoPlay></video>
-        <video style={vidStyle} ref={this.remoteVideoRef} autoPlay></video>
-
-        <br/>
-        <button onClick={this.createOffer}>Offer</button>
-        <button onClick={this.createAnswer}>Answer</button>
-        <br/>
-        <textarea ref={ref=>{this.textref = ref}}/>
-        <br/>
-        <button onClick={this.setRemoteDescription}>Set Remote desc</button>
-        <button onClick={this.addCandidate}>Add candidate</button>
-      </div>
-
-
-    );
-  }
-  
 }
 
 export default App;
---
+++ src/routes/createRoom.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {v1 as uuid} from 'uuid';
+
+const createRoom = (props)=>{
+
+    function create(){
+        const id = uuid();
+        props.history.push(`/room/${id}`);
+    }
+
+    return (
+        <button onClick={create}>Create Room</button>
+    );
+
+}
+
+export default createRoom;
